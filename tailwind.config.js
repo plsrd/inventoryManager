0c0bced2381128d0c3a67f94dc05f96366f6843c
Racheal Pennell <racheal.larimer@gmail.com>
2022-06-18 09:51:34 -0700

    add styling to all items

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,30 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#b197ef',
+
+          secondary: '#1d2682',
+
+          accent: '#52d3a0',
+
+          neutral: '#222B3F',
+
+          'base-100': '#E6E4E7',
+
+          info: '#72D9F8',
+
+          success: '#18C988',
+
+          warning: '#fbbf24',
+
+          error: '#FA0511',
+        },
+      },
+    ],
+  },
   plugins: [require('daisyui')],
 };
